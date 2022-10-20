0935ed2de8668ff9948fa2b2560ef531bc369035
kaifazhehj99 <kaifazhehj99@gmail.com>
2022-10-20 10:55:35 +0900

    jpa_paging

--- src/main/java/com/sparta/springboot/controller/ProductRestController.java
+++ src/main/java/com/sparta/springboot/controller/ProductRestController.java
@@ -13,23 +13,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductRestController {
 
     private final ProductService productService;
 
+    @Autowired
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto,
                                  @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        //스프링 시큐리티를 활용하여 인증,인가를 처리할 때 @AuthenticationPrincipal를 쓰면
-        // 쉽게 UserDetails를 구현하여 만든 Principal 인스턴스를 얻을 수 있다.
         // 로그인 되어 있는 회원 테이블의 ID
         Long userId = userDetails.getUser().getId();
 
         Product product = productService.createProduct(requestDto, userId);
-
         // 응답 보내기
         return product;
     }
@@ -45,18 +53,28 @@ public class ProductRestController {
 
     // 로그인한 회원이 등록한 관심 상품 조회
     @GetMapping("/api/products")
-    public List<Product> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // 로그인 되어 있는 회원 테이블의 ID
+    public Page<Product> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         Long userId = userDetails.getUser().getId();
-
-        return productService.getProducts(userId);
+        page = page - 1;
+        return productService.getProducts(userId, page, size, sortBy, isAsc);
     }
 
-    // (관리자용) 등록된 모든 상품 목록 조회
-    @Secured(value = UserRoleEnum.Authority.ADMIN)
+    // (관리자용) 전체 상품 조회
+    @Secured(UserRoleEnum.Authority.ADMIN)
     @GetMapping("/api/admin/products")
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public Page<Product> getAllProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+    ) {
+        page = page -1;
+        return productService.getAllProducts(page, size, sortBy, isAsc);
     }
-
 }
--- src/main/java/com/sparta/springboot/repository/ProductRepository.java
+++ src/main/java/com/sparta/springboot/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.sparta.springboot.repository;
 
 import com.sparta.springboot.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByUserId(Long userId);
+    Page<Product> findAllByUserId(Long userId, Pageable pageable);
 }
